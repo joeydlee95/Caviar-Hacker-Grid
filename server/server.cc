10dc2ced02b429aa5532c5ecd31692d23d729966
Richard Min <richardmin97@gmail.com>
2017-01-24 00:56:23 -0800

    Some initial commits for vectors

--- server/server.cc
+++ server/server.cc
@@ -30,7 +30,8 @@ void Session::do_read() {
   socket_.async_read_some(boost::asio::buffer(data_, max_length),
     [this, self](boost::system::error_code ec, std::size_t length) {
       if (!ec) {
-        printf("Incoming Data:\n");
+        printf("Incoming Data length %lu:\n", length);
+
         for (std::size_t i = 0; i < length; i++) {
             printf("%c", data_[i]);
         }
