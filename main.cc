f68f69e59c1d798ad4cd989407552ddf6e5e3ed4
Richard Min <richardmin97@gmail.com>
2017-02-19 03:41:48 -0800

    Parsing logic extracted from server

--- main.cc
+++ main.cc
@@ -1,17 +1,23 @@
 #include "webserver.h"
 #include "nginx-configparser/config_parser.h"
+
 int main(int argc, char* argv[]) {
   if (argc != 2) {
     printf("Usage: ./webserver <path to config file>\n");
     return 1;
   }
 
-  NginxConfigParser parser;
-  NginxConfig config;
+  Nginx::NginxConfig config;
 
-  Webserver server(&parser,&config);
+  if(!Nginx::ParseFile(argv[1], &config)) {
+    printf("Syntax Error in NginxConfig %s", argv[1]);
+    return 1;
+  }
 
-  if(!server.run_server(argv[1])){
+  Webserver server(&config);
+
+  if(!server.run_server(argv[1])) {
+    printf("Error running server.");
   	return 1;
   }
   
