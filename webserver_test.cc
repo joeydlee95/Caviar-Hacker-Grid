d20e5e5feb434a586beea0bb29e8efee9297aaaf
Richard Min <richardmin97@gmail.com>
2017-02-20 15:25:16 -0800

    Cleaned up mock class

--- webserver_test.cc
+++ webserver_test.cc
@@ -12,48 +12,36 @@ using ::testing::SetArgReferee;
 using ::testing::An;
 
 
-/*
-class MockNginxConfig: public Nginx::NginxConfig{
+
+class MockNginxConfig : public Nginx::NginxConfig{
 public:
   bool find(const std::string& key, Nginx::NginxConfig& value) const {
     return mocked_find(key, value);
   }
-  bool td::vector<std::string> find(const std::string& key) const {
+  std::vector<std::string> find(const std::string& key) const {
     return mocked_find(key);
   }
-	MOCK_METHOD2(mocked_find,bool(const std::string& key, Nginx::NginxConfig& value));  
-  MOCK_METHOD1(mocked_find,std::vector<std::string>(const std::string& key));
+	MOCK_CONST_METHOD2(mocked_find,bool(const std::string& key, Nginx::NginxConfig& value));  
+  MOCK_CONST_METHOD1(mocked_find,std::vector<std::string>(const std::string& key));
 };
 
-TEST(WebserverTest, ConfigurationFail) {
-  MockNginxConfig mock_config;
-
-  Webserver server(&mock_parser,&mock_config);
-  std::string dummy_file = "dummy";
-  
-  EXPECT_CALL(mock_config, mocked_find("port", mock_config)).Times(0);
-  EXPECT_FALSE(server.run_server(dummy_file.c_str()));
-}
-
+/*
 TEST(WebserverTest, FindPortFail) {
-  
-  MockConfigParser mock_parser;
   MockNginxConfig mock_config;
+  mock_config.children_.emplace_back(new MockNginxConfig);
+  mock_config.children_[0]->tokens_.push_back("dummy");
 
-  Webserver server(&mock_parser,&mock_config);
-  std::string dummy_file = "dummy";
-  
-  EXPECT_CALL(mock_parser, Parse(dummy_file.c_str(), _))
-    .WillOnce(
-      Return(true)
-    );
-  EXPECT_CALL(mock_config, mocked_find("port", An<std::string &>(), An<std::size_t>()))
+  Webserver server(&mock_config);
+  EXPECT_CALL(mock_config, mocked_find("port"))
     .WillOnce(
   	  Return(false)	
     );
-  EXPECT_FALSE(server.run_server(dummy_file.c_str()));
-}
 
+  EXPECT_FALSE(server.Init());
+}
+*/
+
+/*
 TEST(WebserverOptionsTest, ToStringTest) {
   std::unique_ptr<NginxConfig> conf(new MockNginxConfig);
   std::map<std::string, std::vector<std::string> >* options = new std::map<std::string, std::vector<std::string> >;
