170489eccdd2a7142c2c10b701eefa763f502151
Richard Min <richardmin97@gmail.com>
2017-02-20 23:18:31 -0800

    Added comments to config_parser

--- nginx-configparser/config_parser.h
+++ nginx-configparser/config_parser.h
@@ -11,11 +11,19 @@ namespace Nginx {
   // The parsed representation of the entire config.
   class NginxConfig {
   public:
+    // The general ToString method. The top level nginxConfig has no tokens
+    // And only has children, so this doesn't display
+    // Unnecessarily indented blocks
     std::string ToString(int tabLevel = 0) const;
+    // Recursive function that displays full brackets.
     std::string ToStringSubBlock(int tabLevel) const;
 
+    // Finds a key and puts the value into value. Returns true if found.
     virtual bool find(const std::string& key, NginxConfig& value) const;
+    // Finds a key and returns the tokens assosciated with the token
+    // Children block are ignored.
     virtual std::vector<std::string> find(const std::string& key) const;
+    // Finds all keys that match the key.
     virtual std::vector<std::shared_ptr<NginxConfig> > findAll(const std::string& key) const;
 
     std::vector<std::shared_ptr<NginxConfig> > children_;
