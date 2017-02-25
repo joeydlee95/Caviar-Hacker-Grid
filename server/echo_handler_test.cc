2040d89bba2afdfc5f7082ddf1ef5bfe0955fcf0
avikbmohan <avikbmohan@gmail.com>
2017-02-24 16:27:06 -0800

    fixed issue with improper test string.

--- server/echo_handler_test.cc
+++ server/echo_handler_test.cc
@@ -18,6 +18,9 @@ TEST(EchoHandlerTest, BasicTest) {
   auto req = Request::Parse(raw_request);
   Response res;
   EchoHandler eh;
+  std::string expected_string = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nGET /echo HTTP/1.1\r\nHost: localhost:3000\r\n\r\n";
   auto handler_status = eh.HandleRequest(*req, &res);  
   EXPECT_EQ(handler_status, RequestHandler::OK);
+  EXPECT_EQ(res.ToString(), expected_string);
 }
+
