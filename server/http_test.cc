424ca16f035178ff8c6ace994fe1c948a998e3a3
Richard Min <richardmin97@gmail.com>
2017-02-25 01:06:40 -0800

    Removed outdated test

--- server/http_test.cc
+++ server/http_test.cc
@@ -16,21 +16,3 @@ TEST(StatusCode, StatusCodeValidation) {
     }
   }
 }
-
-TEST(HTTPResponse, ToString) {
-  http::HTTPResponse* res = new http::HTTPResponse();
-  http::HTTPResponseBuilder builder(res);
-  
-  EXPECT_TRUE(builder.set_status_code(200));
-  EXPECT_EQ(builder.reason_phrase().reason_phrase_, "OK");
-
-  builder.set_length(10);
-  builder.set_content_type(http::mime_type::CONTENT_TYPE_TEXT_PLAIN);
-  builder.set_body("Cheesy");
-
-  EXPECT_EQ(builder.getResult()->ToString(), "HTTP/1.1 200 OK\r\nContent-Length: 10\r\nContent-Type: text/plain\r\n\r\nCheesy");
-
-  delete res;
-
-}
-
