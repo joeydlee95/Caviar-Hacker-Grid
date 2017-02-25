363f25bbff7f68de503a096b8e60ea9d71b57027
avikbmohan <avikbmohan@gmail.com>
2017-02-24 16:31:11 -0800

    removed debug output

--- server/echo_handler.cc
+++ server/echo_handler.cc
@@ -3,7 +3,6 @@
 #include "httpResponse.h"
 #include "http.h"
 #include <string>
-#include <iostream>
 
 RequestHandler::Status EchoHandler::Init(const std::string& uri_prefix,
 				      const NginxConfig& config) {
@@ -12,11 +11,8 @@ RequestHandler::Status EchoHandler::Init(const std::string& uri_prefix,
 
 RequestHandler::Status EchoHandler::HandleRequest(const Request& request,
 					       Response* response) {
-  std::cout << "A\n";
   response->SetStatus(Response::OK);
-  std::cout << "A\n";
   response->AddHeader("Content-Type", http::mime_type::ContentTypeAsString(http::mime_type::CONTENT_TYPE_TEXT_PLAIN));
-  std::cout << "A\n";
   response->SetBody((request.raw_request()).c_str());
   return OK;
 }
