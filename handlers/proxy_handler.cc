e6c842f9be42a43ee587b2c9e5575fc800e1d203
Evan Krause <evangrayk@gmail.com>
2017-03-01 11:47:30 -0800

    make port optional in proxy handler

--- handlers/proxy_handler.cc
+++ handlers/proxy_handler.cc
@@ -17,7 +17,7 @@ RequestHandler::Status ProxyHandler::Init(const std::string& uri_prefix, const N
 	this->m_uri_prefix_ = uri_prefix;
 	// Assume the config argument has something like:
 	// host www.example.com
-	// port #####.
+	// port #####. (optional)
 	std::vector<std::string> host_config_tokens = config.find("host");
 	if (host_config_tokens.size() != 2){
 		printf("ProxyHandler.Init: Invalid config:\n%s", config.ToString().c_str() );
@@ -26,11 +26,14 @@ RequestHandler::Status ProxyHandler::Init(const std::string& uri_prefix, const N
 	this->m_host_path_ = host_config_tokens[1];
 
 	std::vector<std::string> port_config_tokens = config.find("port");
-	if (port_config_tokens.size() != 2){
+	if (port_config_tokens.size() == 0) {
+		this->m_port_path_ = 80; // default http port
+	} else if (port_config_tokens.size() != 2){
 		printf("ProxyHandler.Init: Invalid config:\n%s", config.ToString().c_str() );
 		return INVALID_CONFIG;
+	} else {
+		this->m_port_path_ = port_config_tokens[1];
 	}
-	this->m_port_path_ = port_config_tokens[1];
 
 	return OK;
 }
