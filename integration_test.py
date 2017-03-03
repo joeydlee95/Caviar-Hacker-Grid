3810f9dea31b4335e83f4c9863f26f1e6ae71dc7
Evan Krause <evangrayk@gmail.com>
2017-03-03 15:48:30 -0800

    update proxy handler integration test

--- integration_test.py
+++ integration_test.py
@@ -5,10 +5,6 @@ import sys
 import os
 import telnetlib
 import time
-## Global Vars ##
-passing = True
-port = 3000;
-#TODO: add a data strx to hold names of failed tests
 
 ## Global Vars ##
 passing = True #Variable to check if all tests passed
@@ -25,6 +21,7 @@ def pas():
     print("PASSED\n")
 def fail(msg):
     print("FAILED: " + str(msg) + "\n")
+    global passing
     passing = False
 
 ## Begin Testing  ##
@@ -59,74 +56,6 @@ if __name__ == "__main__":
 
     time.sleep(3)
 
-def TestProxyHandler():
-    nameTest("ProxyHandler")
-    backend_config = (
-            'port 8080;\n'
-            'path /echo EchoHandler {}\n'
-            'default NotFoundHandler {}\n'
-            )
-    proxy_config = (
-            'port 2020;\n'
-            'path /proxy ProxyHandler {\n'
-            '  host localhost;\n'
-            '  port 8080;\n'
-            '}\n'
-            'default NotFoundHandler {}\n'
-            )
-    backend_config_file = 'test_backend_config'
-    proxy_config_file = 'test_proxy_config'
-    with open(backend_config_file, 'w') as f:
-        f.write(backend_config)
-    with open(proxy_config_file, 'w') as f:
-        f.write(proxy_config)
-
-    WEBSERVER_EXECUTABLE = './webserver'
-    backend_process = subprocess.Popen([WEBSERVER_EXECUTABLE, backend_config_file])
-    proxy_process = subprocess.Popen([WEBSERVER_EXECUTABLE, proxy_config_file])
-    time.sleep(0.1) # give time for webservers to open
-    if backend_process.poll():
-        return fail("backend webserver exited with status %d" % backend_process.returncode)
-    if proxy_process.poll():
-        return fail("proxy webserver exited with status %d" % proxy_process.returncode)
-
-    backend_curl = 'curl -Is localhost:8080/echo | cat'
-    proxy_curl = 'curl -Is localhost:2020/proxy/echo | cat'
-
-    backend_output = subprocess.check_output(backend_curl, shell=True)
-    if backend_output == "":
-        backend_process.terminate()
-        proxy_process.terminate()
-        return fail("backend produced wrong output")
-    proxy_output = subprocess.check_output(proxy_curl, shell=True)
-    if proxy_output == "":
-        backend_process.terminate()
-        proxy_process.terminate()
-        return fail("proxy produced wrong output")
-
-    if (proxy_output != backend_output):
-        fail("Proxy and actual outputs differ!")
-    else:
-        pas()
-
-    backend_process.terminate() # kill server
-    proxy_process.terminate()
-    backend_process.wait() # wait for server to die
-    proxy_process.wait()
-
-    # delete temporary config files
-    os.remove(backend_config_file)
-    os.remove(proxy_config_file)
-
-    line()
-
-TestProxyHandler()
-
-
-
-## Conclusion ##
-print("Passed all tests?\n" + str(passing) + '\n')
-    
 
 ## Notes ##
 # Todo: add/utilize an http response parser that checks for correct
@@ -148,6 +77,74 @@ print("Passed all tests?\n" + str(passing) + '\n')
     ## Testing Complete, Kill Server ##
     print("\nKilling Server.\n")
     serverProcess.kill()
+
+
+#####        
+
+    def TestProxyHandler():
+        nameTest("ProxyHandler")
+        backend_config = (
+                'port 8080;\n'
+                'path /echo EchoHandler {}\n'
+                'default NotFoundHandler {}\n'
+                )
+        proxy_config = (
+                'port 8081;\n'
+                'path /proxy ProxyHandler {\n'
+                '  host localhost;\n'
+                '  port 8080;\n'
+                '}\n'
+                'default NotFoundHandler {}\n'
+                )
+        backend_config_file = 'test_backend_config'
+        proxy_config_file = 'test_proxy_config'
+        with open(backend_config_file, 'w') as f:
+            f.write(backend_config)
+        with open(proxy_config_file, 'w') as f:
+            f.write(proxy_config)
+
+        WEBSERVER_EXECUTABLE = './webserver'
+        backend_process = subprocess.Popen([WEBSERVER_EXECUTABLE, backend_config_file])
+        proxy_process = subprocess.Popen([WEBSERVER_EXECUTABLE, proxy_config_file])
+        time.sleep(0.2) # give time for webservers to open
+        try:
+            if backend_process.poll():
+                return fail("backend webserver exited with status %d" % backend_process.returncode)
+            if proxy_process.poll():
+                return fail("proxy webserver exited with status %d" % proxy_process.returncode)
+
+            backend_curl = 'curl -Is localhost:8080/echo | cat'
+            proxy_curl = 'curl -Is localhost:8081/proxy/echo | cat'
+
+            backend_output = subprocess.check_output(backend_curl, shell=True)
+            if backend_output == "":
+                backend_process.terminate()
+                proxy_process.terminate()
+                return fail("backend produced wrong output")
+            proxy_output = subprocess.check_output(proxy_curl, shell=True)
+            if proxy_output == "":
+                backend_process.terminate()
+                proxy_process.terminate()
+                return fail("proxy produced wrong output")
+
+            if (proxy_output != backend_output):
+                fail("Proxy and actual outputs differ!")
+            else:
+                pas()
+        finally:
+            backend_process.terminate() # kill server
+            proxy_process.terminate()
+            backend_process.wait() # wait for server to die
+            proxy_process.wait()
+
+            # delete temporary config files
+            os.remove(backend_config_file)
+            os.remove(proxy_config_file)
+
+        line()
+
+    TestProxyHandler()
+
 
 #####        
     
